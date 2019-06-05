826c4fd9a43f19742c3adf3f30dd290f1e4cd804
Greg C <gconrad0922@gmail.com>
2019-06-05 08:25:27 -0400

    Add vue

---
+++ meal-prep/src/components/CustomComp.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>{{ newHere }}</h1>
+</template>
+<script>
+    export default {
+        name: 'CustomComp',
+        props: {
+            newHere: String
+        }
+    };
+</script>
